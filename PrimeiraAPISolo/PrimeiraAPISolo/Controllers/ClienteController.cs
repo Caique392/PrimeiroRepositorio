7e9855df5ba4ef0282e6f51f24bc68c68a80eb48
Caique392 <caique39@outlook.com>
2022-07-31 04:16:15 -0300

    Criação Projeto Primeira API

---
+++ PrimeiraAPISolo/PrimeiraAPISolo/Controllers/ClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrimeiraAPISolo.Models;
+
+namespace PrimeiraAPISolo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private static List<ClienteModel> clientes = new List<ClienteModel>();
+
+        [HttpGet()]
+        public List<ClienteModel> Get()
+        {
+            return clientes;
+        }
+       [HttpPost()]
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            clientes.Add(new ClienteModel(nome));
+        }
+
+        [HttpDelete]
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(X => X.Cliente.Equals(nome))));
+        }
+    }
+}
---
+++ PrimeiraAPISolo/PrimeiraAPISolo/Models/ClienteModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeiraAPISolo.Models
+{
+    public class ClienteModel
+    {
+        public string Cliente { get; set; }
+
+        public ClienteModel(string nome)
+        {
+            this.Cliente = nome;
+        }
+    }
+}
